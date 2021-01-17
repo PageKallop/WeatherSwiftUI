caafcb465ec4a3d6b66983516190a990113ca0b0
PageKallop <PageMarie78@gmail.com>
2021-01-17 15:50:05 -0600

    Created url session

--- WeatherSwiftUI/Models/Services/WeatherService.swift
+++ WeatherSwiftUI/Models/Services/WeatherService.swift
@@ -11,10 +11,28 @@ class WeatherService {
     
     func getWeather(city: String, completion: @escaping (Weather?) -> ()) {
         
+        //creates url string
         guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=&units=imperial") else {
             completion(nil)
             return 
         }
+        //creates url session
+        URLSession.shared.dataTask(with: url) { (data, responce, error) in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            //returns the weather from the session
+            let weatherResponce = try? JSONDecoder().decode(WeatherModels.self, from: data)
+            
+            if let weatherResponce = weatherResponce {
+                let weather = weatherResponce.main
+                completion(weather)
+            } else {
+                completion(nil)
+            }
+        } .resume()
+        
     }
 }
 
